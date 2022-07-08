1819011eddf7a28c572049b5e61b32d11729b519
Andres Saenz <asaenzcordero@gmail.com>
2022-07-08 11:36:47 -0500

    Kata5 Resolve

--- src/main/java/katas/Kata5.java
+++ src/main/java/katas/Kata5.java
@@ -1,14 +1,10 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import model.BoxArt;
-import model.Movie;
-import model.MovieList;
-import util.DataUtil;
 
+import model.Movie;
+import util.DataUtil;
 import java.util.List;
-import java.util.Map;
+
 
 /*
     Goal: Retrieve the largest rating using reduce()
@@ -16,9 +12,14 @@ import java.util.Map;
     Output: Double
 */
 public class Kata5 {
+    private Kata5() {
+
+    }
     public static Double execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return 3.0;
+        return movies.stream().map(Movie::getRating)
+                .reduce(Double::max)
+                .orElseThrow();
     }
 }
