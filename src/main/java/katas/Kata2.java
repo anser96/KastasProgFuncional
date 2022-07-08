38d35679f7bad69395cd774d156ed1d806178403
Andres Saenz <asaenzcordero@gmail.com>
2022-07-08 11:08:06 -0500

    Kata2 Resolve

--- src/main/java/katas/Kata2.java
+++ src/main/java/katas/Kata2.java
@@ -7,6 +7,7 @@ import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Chain filter() and map() to collect the ids of videos that have a rating of 5.0
@@ -14,9 +15,23 @@ import java.util.Map;
     Output: List of Integers
 */
 public class Kata2 {
+    private Kata2() {
+
+    }
     public static List<Integer> execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return ImmutableList.of(1, 2, 3);
+
+        return movies
+                .stream()
+                .filter(
+                        movie -> movie.getRating().equals(5.0)
+                )
+                .map(
+                        Movie::getId
+                )
+                .collect(
+                        Collectors.toList()
+                );
     }
 }
