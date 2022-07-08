4b5245403716fe202ee08842a0759b66123cccc3
Andres Saenz <asaenzcordero@gmail.com>
2022-07-08 12:23:43 -0500

    Kata9 Resolve

--- src/main/java/katas/Kata9.java
+++ src/main/java/katas/Kata9.java
@@ -1,15 +1,11 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import model.Bookmark;
+
 import model.Movie;
 import model.MovieList;
 import util.DataUtil;
-
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Stream;
 
 /*
     Goal: Retrieve each video's id, title, middle interesting moment time, and smallest box art url
@@ -20,6 +16,25 @@ public class Kata9 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "some title", "time", new Date(), "url", "someUrl"));
+        List<Map> list = new ArrayList<>();
+        for (MovieList movieList : movieLists) {
+            for (Movie movie : movieList.getVideos()) {
+                Map<String, Object> id = Map.of(
+                        "id", movie.getId(), "title", movie.getTitle(),
+                        "time", movie.getInterestingMoments().stream()
+                                .flatMap(interestingMoment -> Stream.of(interestingMoment.getTime()))
+                                .mapToLong(Date::getTime)
+                                .average()
+                                .stream().mapToObj(prom -> new Date((long) prom)),
+                        "url", movie.getBoxarts()
+                                .stream()
+                                .min(Comparator.comparing(boxArt -> boxArt.getUrl().length()))
+                                .orElseThrow().getUrl()
+                );
+                list.add(id);
+            }
+        }
+        return list;
+
     }
 }
