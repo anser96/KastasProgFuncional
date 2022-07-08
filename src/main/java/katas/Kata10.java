87873fbdada29860559b7082503f2cf065adbe2d
Andres Saenz <asaenzcordero@gmail.com>
2022-07-08 12:31:00 -0500

    Kata10 Resolve

--- src/main/java/katas/Kata10.java
+++ src/main/java/katas/Kata10.java
@@ -8,6 +8,9 @@ import util.DataUtil;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.google.common.collect.ImmutableMap.*;
 
 /*
     Goal: Create a datastructure from the given data:
@@ -55,9 +58,20 @@ public class Kata10 {
         List<Map> lists = DataUtil.getLists();
         List<Map> videos = DataUtil.getVideos();
 
-        return ImmutableList.of(ImmutableMap.of("name", "someName", "videos", ImmutableList.of(
-                ImmutableMap.of("id", 5, "title", "The Chamber"),
-                ImmutableMap.of("id", 3, "title", "Fracture")
-        )));
+        List<Map> collect = lists.stream()
+                .map(map -> of(
+                        "name", map.get("name"), "videos",
+                        videos.stream()
+                                .filter(map1 -> map.get("id").equals(map1.get("listId")))
+                                .map(Kata10::apply).collect(Collectors.toList())
+                )).collect(Collectors.toList());
+        return collect;
+    }
+
+    private static ImmutableMap<String, Object> apply(Map map1) {
+        return of(
+                "id", map1.get("id"),
+                "title", map1.get("title")
+        );
     }
 }
